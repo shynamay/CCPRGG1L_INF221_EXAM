344427406ee142fdfb071990aada0b2092dd715f
Sheena Mae Pader <111573118+shynamay@users.noreply.github.com>
2022-09-05 09:47:06 +0800

    Add files via upload

---
+++ Exam_1/Person.java
@@ -0,0 +1,20 @@
+public class Person {
+    String surname;
+String firstname;
+char middleInitial;
+int dateOfBirth;
+int studentNumber;
+String studentEmailAddress;
+boolean iAmAwesome;
+
+ // Character method
+ public void sayMyStudentNumber() {
+    System.out.println("Student Numer" + studentNumber );
+}
+public void sayMyEmailAddress() {
+    System.out.println("StudentEmailAddress" + studentEmailAddress );
+}
+public void amIAwesome() {
+    System.out.println("iAmAwesome" + iAmAwesome );
+}
+}
---
+++ Exam_1/Student.java
@@ -0,0 +1,18 @@
+public class Student {
+        public static void main(String[] args) {
+         // Properties
+        
+         Person Me = new Person();
+         Me.surname = "Pader";
+         Me.firstname = "Sheena Mae";
+         Me.middleInitial = 'O';
+         Me.dateOfBirth = 8/13/2003;
+         Me.studentNumber = 101082;
+         Me.studentEmailAddress = "paderso@students.national-u.edu.ph";
+         Me.iAmAwesome = true;
+         Me.sayMyStudentNumber();
+         Me.sayMyEmailAddress();
+         Me.amIAwesome();
+        }
+
+}
